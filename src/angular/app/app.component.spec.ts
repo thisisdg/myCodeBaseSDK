c46ac7416e89bb8ebe987487a00fa50bd2ffc766
Dhruv Goel <thisisdg@rediffmail.com>
2020-09-02 23:37:12 +0530

    Resolved the bugs in the angular components compilation according to the new folder structure

--- src/angular/app/app.component.spec.ts
+++ src/angular/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trial'`, () => {
+  it(`should have as title 'my-code-base-sdk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trial');
+    expect(app.title).toEqual('my-code-base-sdk');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trial app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('my-code-base-sdk app is running!');
   });
 });
--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trial';
+  title = 'my-code-base-sdk';
 }
